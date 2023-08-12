50633d104384ea7a23f5593c3c909c9972edf391
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-08-12 03:31:39 +0000

    feat: update footer with links

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,46 +5,104 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h2 className={styles.footer__main}>PersonalizeIt</h2>
-      <p className={styles.footer__copyright}>©SAI SHRAVAN 2023</p>
-      <ul className={styles.footer__list}>
-        <li className={styles.footer__listItem}>
-          <Link
-            href='https://www.github.com/shravzzv'
-            className={styles.footer__link}
-            target='_blank'
-          >
-            GitHub
-          </Link>
-        </li>
-        <li className={styles.footer__listItem}>
-          <Link
-            href='https://www.instagram.com/shravzzv'
-            className={styles.footer__link}
-            target='_blank'
-          >
-            Instagram
-          </Link>
-        </li>
-        <li className={styles.footer__listItem}>
-          <Link
-            href='https://www.linkedin.com/in/sai-shravan-027040244/'
-            className={styles.footer__link}
-            target='_blank'
-          >
-            LinkedIn
-          </Link>
-        </li>
-        <li className={styles.footer__listItem}>
-          <Link
-            href='https://www.youtube.com/@shravzzv'
-            className={styles.footer__link}
-            target='_blank'
-          >
-            YouTube
-          </Link>
-        </li>
-      </ul>
+      <main className={styles.footer__main}>
+        <h1 className={styles.footer__logo}>PersonalizeIt</h1>
+        <p className={styles.footer__desc}>
+          Tailor mad templates for productivity.
+        </p>
+        <p className={styles.footer__copyright}>&copy; Sai Shravan Vadla</p>
+      </main>
+
+      <hr className={styles.hr} />
+
+      <section className={styles.footer__section}>
+        <ul className={styles.footer__list}>
+          <li className={styles.footer__listItem}>
+            <Link
+              href='https://personalizeit.vercel.app/'
+              className={styles.footer__link}
+              target='_blank'
+            >
+              PersonalizeIt
+            </Link>
+          </li>
+          <li className={styles.footer__listItem}>
+            <Link
+              href='https://personalizeit.vercel.app/'
+              className={styles.footer__link}
+              target='_blank'
+            >
+              Home
+            </Link>
+          </li>
+          <li className={styles.footer__listItem}>
+            <Link
+              href='https://personalizeit.vercel.app/taskstemplate'
+              className={styles.footer__link}
+              target='_blank'
+            >
+              Templates
+            </Link>
+          </li>
+          <li className={styles.footer__listItem}>
+            <Link
+              href='https://personalizeit.vercel.app/taskstemplate'
+              className={styles.footer__link}
+              target='_blank'
+            >
+              Tasks template
+            </Link>
+          </li>
+        </ul>
+
+        <ul className={styles.footer__list}>
+          <li className={styles.footer__listItem}>
+            <Link
+              href='https://saishravan.vercel.app/'
+              target='_blank'
+              className={styles.footer__link}
+            >
+              Contact me
+            </Link>
+          </li>
+          <li className={styles.footer__listItem}>
+            <Link
+              href='https://github.com/shravzzv'
+              target='_blank'
+              className={styles.footer__link}
+            >
+              GitHub
+            </Link>
+          </li>
+          <li className={styles.footer__listItem}>
+            <Link
+              href='https://www.linkedin.com/in/sai-shravan-027040244/'
+              target='_blank'
+              className={styles.footer__link}
+            >
+              LinkedIn
+            </Link>
+          </li>
+          <li className={styles.footer__listItem}>
+            <Link
+              href='https://instagram.com/shravzzv'
+              target='_blank'
+              className={styles.footer__link}
+            >
+              Instagram
+            </Link>
+          </li>
+          <li className={styles.footer__listItem}>
+            <Link
+              href='https://www.youtube.com/@shravzzv'
+              target='_blank'
+              className={styles.footer__link}
+            >
+              YouTube
+            </Link>
+          </li>
+        </ul>
+      </section>
     </footer>
   )
 }
